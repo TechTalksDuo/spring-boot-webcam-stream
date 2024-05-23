b6b213ade440ee690bcd8576636521268cd8c5fc
mihaita-tinta <mihaita.tinta@gmail.com>
2024-05-23 15:41:54 +0300

    enable cors

--- src/main/java/com/example/demo/websocket/WebSocketConfig.java
+++ src/main/java/com/example/demo/websocket/WebSocketConfig.java
@@ -31,6 +31,7 @@ public class WebSocketConfig implements WebSocketConfigurer {
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         registry.addHandler(new WebSocketHandler(), "/websocket")
+                .setAllowedOriginPatterns("*")
                 .addInterceptors(new HttpSessionHandshakeInterceptor());
     }
 
