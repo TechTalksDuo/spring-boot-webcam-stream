1cf592aaa83f7457ff07a2834501ff489aa99058
Bogdan Arvinte <bogdan.arvinte@outlook.com>
2024-05-31 13:51:25 +0300

    chore: update llm service to use GPU

--- .docker/local-llm/app/llm.py
+++ .docker/local-llm/app/llm.py
@@ -1,7 +1,7 @@
 # Use a pipeline as a high-level helper
 from transformers import pipeline
 
-pipe = pipeline("image-classification", model="MahmoudWSegni/swin-tiny-patch4-window7-224-finetuned-face-emotion-v12_right")
+pipe = pipeline("image-classification", model="MahmoudWSegni/swin-tiny-patch4-window7-224-finetuned-face-emotion-v12_right", device_map="auto")
 
 def run_inference(payload):
   result = pipe(payload)
